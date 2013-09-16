28cd14af3824c02a706739e3dba5094b34a56504
Shisei Hanai <shanai@ruimo.com>
2013-09-16 15:24:06 +0900

    Compile error.

--- app/models/ChangeItemPrice.scala
+++ app/models/ChangeItemPrice.scala
@@ -4,28 +4,28 @@ import play.api.db.DB
 import play.api.Play.current
 
 case class ChangeItemPriceTable(
-  itemDescriptions: Seq[ChangeItemDescription]
+  itemPrices: Seq[ChangeItemPrice]
 ) {
   def update(itemId: Long) {
-    itemDescriptions.foreach {
+    itemPrices.foreach {
       _.update(itemId)
     }
   }
 }
 
-case class ChangeItemDescription(
-  siteId: Long, localeId: Long, itemDescription: String
+case class ChangeItemPrice(
+  siteId: Long, localeId: Long, itemPrice: BigDecimal
 ) {
   def update(itemId: Long) {
     DB.withTransaction { implicit conn =>
-      ItemDescription.update(siteId, itemId, localeId, itemDescription)
+//      ItemPrice.update(siteId, itemId, localeId, itemPrice)
     }
   }
 
   def add(itemId: Long) {
     ExceptionMapper.mapException {
       DB.withTransaction { implicit conn =>
-        ItemDescription.add(siteId, itemId, localeId, itemDescription)
+//        ItemPrice.add(siteId, itemId, localeId, itemPrice)
       }
     }
   }
