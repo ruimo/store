4bfea6bb9a26755ca787330c29896547c18f6b54
Ruimo Uno <ruimo.uno@gmail.com>
2013-09-25 22:53:44 +0900

    paging

--- app/models/PagedRecords.scala
+++ app/models/PagedRecords.scala
@@ -1,12 +1,12 @@
 package models
 
 case class PagedRecords[T] (
-  currentPage: Int,
-  pageSize: Int,
-  pageCount: Long,
+  currentPage: Int, // zero is the first page.
+  pageSize: Int,    // the number of items in one page.
+  pageCount: Long,  // the number of pages.
   records: Seq[T]
 ) {
-  lazy val nextPageExists = pageSize < pageCount
+  lazy val nextPageExists = currentPage + 1 < pageCount
   lazy val prevPageExists = currentPage > 0
 }
 
