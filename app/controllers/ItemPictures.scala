0d2df55e075756c5b979d7af5254a4026a41d4e1
Ruimo Uno <ruimo.uno@gmail.com>
2013-10-27 17:38:13 +0900

    refs #64

--- app/controllers/ItemPictures.scala
+++ app/controllers/ItemPictures.scala
@@ -44,6 +44,34 @@ object ItemPictures extends Controller with I18nAware with NeedLogin with HasLog
     }
   }
 
+  def uploadDetail(itemId: Long) = Action(parse.multipartFormData) { implicit request =>
+    retrieveLoginSession(request) match {
+      case None => onUnauthorized(request)
+      case Some(user) =>
+        request.body.file("picture").map { picture =>
+          val filename = picture.filename
+          val contentType = picture.contentType
+          if (contentType != Some("image/jpeg")) {
+            Redirect(
+              routes.ItemMaintenance.startChangeItem(itemId)
+            ).flashing("errorMessage" -> Messages("jpeg.needed"))
+          }
+          else {
+            picture.ref.moveTo(toDetailPath(itemId).toFile, true)
+            Redirect(
+              routes.ItemMaintenance.startChangeItem(itemId)
+            ).flashing("message" -> Messages("itemIsUpdated"))
+          }
+        }.getOrElse {
+          Redirect(routes.ItemMaintenance.startChangeItem(itemId)).flashing(
+            "errorMessage" -> Messages("file.not.found")
+          )
+        }.withSession(
+          request.session + (LoginUserKey -> user.withExpireTime(System.currentTimeMillis + SessionTimeout).toSessionString)
+        )
+    }
+  }
+
   def getPicture(itemId: Long, no: Int) = Action { request =>
     val path = toPath(itemId, no)
     if (Files.isReadable(path)) {
@@ -54,6 +82,22 @@ object ItemPictures extends Controller with I18nAware with NeedLogin with HasLog
     }
     else {
       val source = Source.fromFile(picturePath.resolve("notfound.jpg").toFile)(scala.io.Codec.ISO8859)
+      val byteArray = source.map(_.toByte).toArray
+      source.close()
+      Ok(byteArray).as("image/jpeg")
+    }
+  }
+
+  def getDetailPicture(itemId: Long) = Action { request =>
+    val path = toDetailPath(itemId)
+    if (Files.isReadable(path)) {
+      val source = Source.fromFile(path.toFile)(scala.io.Codec.ISO8859)
+      val byteArray = source.map(_.toByte).toArray
+      source.close()
+      Ok(byteArray).as("image/jpeg")
+    }
+    else {
+      val source = Source.fromFile(picturePath.resolve("detailnotfound.jpg").toFile)(scala.io.Codec.ISO8859)
       val byteArray = source.map(_.toByte).toArray
       source.close()
       Ok(byteArray).as("image/jpeg")
@@ -73,5 +117,19 @@ object ItemPictures extends Controller with I18nAware with NeedLogin with HasLog
     ).flashing("message" -> Messages("itemIsUpdated"))
   }
 
+  def removeDetail(itemId: Long) = Action { implicit request =>
+    try {
+      Files.delete(toDetailPath(itemId))
+    }
+    catch {
+      case e: NoSuchFileException =>
+      case e => throw e
+    }
+    Redirect(
+      routes.ItemMaintenance.startChangeItem(itemId)
+    ).flashing("message" -> Messages("itemIsUpdated"))
+  }
+
   def toPath(itemId: Long, no: Int) = picturePath.resolve(itemId + "_" + no + ".jpg")
+  def toDetailPath(itemId: Long) = picturePath.resolve("detail" + itemId + ".jpg")
 }
