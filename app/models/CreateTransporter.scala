5c208694b98c1dc2c96e4867a0d3abbb30d96837
Ruimo Uno <ruimo.uno@gmail.com>
2013-12-02 23:34:27 +0900

    Bug when creating duplicated name of transporter.

--- app/models/CreateTransporter.scala
+++ app/models/CreateTransporter.scala
@@ -2,10 +2,11 @@ package models
 
 import play.api.db.DB
 import play.api.Play.current
+import java.sql.Connection
 
 case class CreateTransporter(localeId: Long, transporterName: String) {
-  def save() {
-    DB.withConnection { implicit conn =>
+  def save(implicit conn: Connection) {
+    ExceptionMapper.mapException {
       val trans = Transporter.createNew
       TransporterName.createNew(trans.id.get, LocaleInfo(localeId), transporterName)
     }
