21858f71f0495912110dd04a1d9e712fe2cbbb80
Shisei Hanai <ruimo.uno@gmail.com>
2014-02-09 11:57:55 +0900

    Add login session to csv header.

--- app/views/csv/TransactionDetailCsv.scala
+++ app/views/csv/TransactionDetailCsv.scala
@@ -1,10 +1,14 @@
 package views.csv
 
+import models.LoginSession
 import play.api.i18n.{Lang, Messages}
 import helpers.Csv
 
 object TransactionDetailCsv {
-  def instance(implicit lang: Lang) = new Csv(
+  def instance(
+    implicit lang: Lang,
+    loginSession: LoginSession
+  ) = new Csv(
     Messages("csv.tran.detail.id"),
     Messages("csv.tran.detail.date"),
     Messages("csv.tran.detail.shippingDate"),
