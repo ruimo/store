30508bf249c410c3bcf27515658411afc69b555f
Shisei Hanai <ruimo.uno@gmail.com>
2014-02-23 13:07:00 +0900

    Item detail template.

--- app/controllers/ItemDetail.scala
+++ app/controllers/ItemDetail.scala
@@ -2,16 +2,31 @@ package controllers
 
 import play.api._
 import db.DB
+import libs.json.{JsObject, Json}
 import play.api.mvc._
 
-import models.Item
-import models.LocaleInfo
+import models.{SiteItemNumericMetadataType, Item, LocaleInfo}
 import play.api.Play.current
 import controllers.I18n.I18nAware
 
 object ItemDetail extends Controller with I18nAware with NeedLogin {
   def show(itemId: Long, siteId: Long) = Action { implicit request => DB.withConnection { implicit conn => {
     implicit val login = loginSession(request, conn)
-    Ok(views.html.itemDetail(models.ItemDetail.show(siteId, itemId, LocaleInfo.byLang(lang))))
+    val itemDetail = models.ItemDetail.show(siteId, itemId, LocaleInfo.byLang(lang))
+    itemDetail.siteItemNumericMetadata.get(SiteItemNumericMetadataType.ITEM_DETAIL_TEMPLATE) match {
+      case None => Ok(views.html.itemDetail(itemDetail))
+      case Some(metadata) => 
+        if (metadata.metadata == 0) Ok(views.html.itemDetail(itemDetail))
+        else Ok(views.html.itemDetailTemplate(metadata.metadata, itemDetail))
+    }
   }}}
+
+  def showAsJson(itemId: Long, siteId: Long) = Action { implicit request => DB.withConnection { implicit conn => {
+    implicit val login = loginSession(request, conn)
+    Ok(asJson(models.ItemDetail.show(siteId, itemId, LocaleInfo.byLang(lang))))
+  }}}
+
+  def asJson(detail: models.ItemDetail): JsObject = Json.obj(
+    "name" -> detail.name
+  )
 }
