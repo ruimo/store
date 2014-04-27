a36b020eb345f0499330d03fc5a5237631659e05
Shisei Hanai <ruimo.uno@gmail.com>
2014-04-27 16:24:23 +0900

    Accouting Bill.

--- app/controllers/AccountingBill.scala
+++ app/controllers/AccountingBill.scala
@@ -2,12 +2,12 @@ package controllers
 
 import play.api.Play.current
 import play.api.data.Forms._
-import play.api.mvc.Controller
 import controllers.I18n.I18nAware
 import models._
 import play.api.data.Form
 import play.api.db.DB
 import collection.immutable.LongMap
+import play.api.mvc.{Controller, RequestHeader}
 
 object AccountingBill extends Controller with NeedLogin with HasLogger with I18nAware {
   val accountingBillForm = Form(
@@ -19,7 +19,9 @@ object AccountingBill extends Controller with NeedLogin with HasLogger with I18n
 
   def index() = isAuthenticated { implicit login => implicit request =>
     Ok(
-      views.html.accountingBill(accountingBillForm, List(), LongMap(), LongMap(), LongMap(), false)
+      views.html.accountingBill(
+        accountingBillForm, accountingBillForm, List(), LongMap(), LongMap(), LongMap(), false
+      )
     )
   }
 
@@ -27,46 +29,67 @@ object AccountingBill extends Controller with NeedLogin with HasLogger with I18n
     accountingBillForm.bindFromRequest.fold(
       formWithErrors => {
         BadRequest(
-          views.html.accountingBill(formWithErrors, List(), LongMap(), LongMap(), LongMap(), false)
+          views.html.accountingBill(formWithErrors, accountingBillForm, List(), LongMap(), LongMap(), LongMap(), false)
         )
       },
       yearMonth => {
-        DB.withConnection { implicit conn =>
-          val summaries = TransactionSummary.listByPeriod(
-            siteUser = login.siteUser, yearMonth = yearMonth
-          )
-          val siteTranByTranId = summaries.foldLeft(LongMap[PersistedTransaction]()) {
-            (sum, e) =>
-            val siteTran = (new TransactionPersister).load(e.transactionId, LocaleInfo.getDefault)
-            sum.updated(e.transactionId, siteTran)
-          }
-          val detailByTranSiteId = summaries.foldLeft(LongMap[Seq[TransactionDetail]]()) {
-            (sum, e) =>
-            val details = TransactionDetail.show(e.transactionSiteId, LocaleInfo.byLang(lang))
-            sum.updated(e.transactionSiteId, details)
-          }
-          val boxBySiteAndItemSize = summaries.foldLeft(
-            LongMap[LongMap[TransactionLogShipping]]()
-          ) {
-            (sum, e) =>
-              sum ++ TransactionLogShipping.listBySite(e.transactionSiteId).foldLeft(
-                LongMap[LongMap[TransactionLogShipping]]().withDefaultValue(LongMap[TransactionLogShipping]())
-              ) {
-                (names, e2) =>
-                  names.updated(
-                    e.transactionSiteId,
-                    names(e.transactionSiteId).updated(e2.itemClass, e2)
-                  )
-              }
-          }
-
-          Ok(views.html.accountingBill(
-            accountingBillForm.fill(yearMonth),
-            summaries, detailByTranSiteId, boxBySiteAndItemSize, siteTranByTranId,
-            login.siteUser.isDefined
-          ))
-        }
+        showCommon(yearMonth, false)
       }
     )
+  }
+  def showForStore() = isAuthenticated { implicit login => implicit request =>
+    accountingBillForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(
+          views.html.accountingBill(accountingBillForm, formWithErrors, List(), LongMap(), LongMap(), LongMap(), false)
+        )
+      },
+      yearMonth => {
+        showCommon(yearMonth, true)
+      }
+    )
+  }
+
+  def showCommon(
+    yearMonth: YearMonth, useCostPrice: Boolean
+  )(
+    implicit login: LoginSession, request: play.api.mvc.Request[_]
+  ) = {
+    DB.withConnection { implicit conn =>
+      val summaries = TransactionSummary.listByPeriod(
+        siteUser = login.siteUser, yearMonth = yearMonth
+      )
+      val siteTranByTranId = summaries.foldLeft(LongMap[PersistedTransaction]()) {
+        (sum, e) =>
+        val siteTran = (new TransactionPersister).load(e.transactionId, LocaleInfo.getDefault)
+        sum.updated(e.transactionId, siteTran)
+      }
+      val detailByTranSiteId = summaries.foldLeft(LongMap[Seq[TransactionDetail]]()) {
+        (sum, e) =>
+        val details = TransactionDetail.show(e.transactionSiteId, LocaleInfo.byLang(lang))
+        sum.updated(e.transactionSiteId, details)
+      }
+      val boxBySiteAndItemSize = summaries.foldLeft(
+        LongMap[LongMap[TransactionLogShipping]]()
+      ) {
+        (sum, e) =>
+        sum ++ TransactionLogShipping.listBySite(e.transactionSiteId).foldLeft(
+          LongMap[LongMap[TransactionLogShipping]]().withDefaultValue(LongMap[TransactionLogShipping]())
+        ) {
+          (names, e2) =>
+          names.updated(
+            e.transactionSiteId,
+            names(e.transactionSiteId).updated(e2.itemClass, e2)
+          )
+        }
+      }
+
+      Ok(views.html.accountingBill(
+        accountingBillForm.fill(yearMonth),
+        accountingBillForm.fill(yearMonth),
+        summaries, detailByTranSiteId, boxBySiteAndItemSize, siteTranByTranId,
+        useCostPrice
+      ))
+    }
   }
 }
