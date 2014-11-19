b106cab27e9db5dda6678f280bbe207a967f0348
Shisei Hanai <ruimo.uno@gmail.com>
2014-11-19 19:54:12 +0900

    Add test.

--- test/functional/ResetPasswordSpec.scala
+++ test/functional/ResetPasswordSpec.scala
@@ -4,6 +4,7 @@ import play.api.test._
 import play.api.test.Helpers._
 import play.api.Play.current
 import java.sql.Connection
+import java.util.concurrent.TimeUnit
 
 import helpers.Helper._
 
@@ -24,7 +25,7 @@ class ResetPasswordSpec extends Specification {
   "Reset password" should {
     "Can reset password" in {
       val app = FakeApplication(additionalConfiguration = conf)
-      running(TestServer(3333, app), Helpers.HTMLUNIT) { browser => DB.withConnection { implicit conn =>
+      running(TestServer(3333, app), Helpers.FIREFOX) { browser => DB.withConnection { implicit conn =>
         implicit val lang = Lang("ja")
         val salt = RandomTokenGenerator().next
         val hash = PasswordHash.generate("password", salt)
@@ -34,7 +35,26 @@ class ResetPasswordSpec extends Specification {
           hash, salt, UserRole.NORMAL, Some("companyName")
         )
 
-        
+        browser.goTo(
+          "http://localhost:3333" + controllers.routes.UserEntry.resetPasswordStart() + "?lang=" + lang.code
+        )
+
+        browser.title === Messages("resetPassword")
+
+        // Empty input
+        browser.find("#doResetPasswordButton").click()
+        browser.await().atMost(5, TimeUnit.SECONDS).untilPage().isLoaded()
+        browser.find(".globalErrorMessage").getText === Messages("inputError")
+        browser.find("#userName_field dd.error").getText === Messages("error.required")
+
+        // Wrong user name
+        browser.fill("#userName").`with`("userName2")
+        browser.find("#doResetPasswordButton").click()
+        browser.await().atMost(5, TimeUnit.SECONDS).untilPage().isLoaded()
+        browser.find(".globalErrorMessage").getText === Messages("inputError")
+        browser.find("#userName_field dd.error").getText === Messages("error.value")
+
+        Thread.sleep(60000)
       }}
     }
   }
