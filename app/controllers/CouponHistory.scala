14e49f4813a4ed262ad352de4eace6a9b00b3736
Shisei Hanai <ruimo.uno@gmail.com>
2015-04-11 10:58:40 +0900

    Instant coupon

--- app/controllers/CouponHistory.scala
+++ app/controllers/CouponHistory.scala
@@ -6,7 +6,7 @@ import db.DB
 import play.api.mvc._
 import play.api.Play.current
 import controllers.I18n.I18nAware
-import models.{CouponDetail, TransactionLogCoupon, LocaleInfo, TransactionLogCouponId, SiteItemNumericMetadataType}
+import models.{CouponDetail, TransactionLogCoupon, LocaleInfo, TransactionLogCouponId, SiteItemNumericMetadataType, CouponDetailWithMetadata, LoginSession, SiteItemNumericMetadata, ItemId}
 
 object CouponHistory extends Controller with I18nAware with NeedLogin {
   def showPurchasedCouponList(
@@ -31,16 +31,41 @@ object CouponHistory extends Controller with I18nAware with NeedLogin {
       val couponDetail = TransactionLogCoupon.at(
         LocaleInfo.byLang(lang), login.userId, TransactionLogCouponId(tranCouponId)
       )
-      couponDetail.siteItemNumericMetadata.get(SiteItemNumericMetadataType.COUPON_TEMPLATE) match {
-        case None => Ok(views.html.showCoupon(couponDetail))
-        case Some(metadata) =>
-          if (metadata.metadata == 0) Ok(views.html.showCoupon(couponDetail))
-          else Ok(views.html.showCouponTemplate(metadata.metadata, couponDetail))
-      }
+      showCoupon(couponDetail)
     }
   }
 
   def showInstantCoupon(siteId: Long, itemId: Long) = isAuthenticated { implicit login => implicit request =>
-    Ok("")
+    val metaData: Map[SiteItemNumericMetadataType, SiteItemNumericMetadata] = 
+      DB.withConnection { implicit conn => SiteItemNumericMetadata.all(siteId, ItemId(itemId)) }
+
+    if (metaData.get(SiteItemNumericMetadataType.INSTANT_COUPON).getOrElse(0) != 0) {
+      val templateNo = metaData.get(SiteItemNumericMetadataType.COUPON_TEMPLATE).map(_.metadata).getOrElse(0)
+      if (templateNo == 0) {
+//        Ok(views.html.showCoupon(couponDetail))
+Ok("")
+      }
+      else {
+//        Ok(views.html.showCouponTemplate(templateNo, couponDetail))
+Ok("")
+      }
+    }
+    else {
+      Redirect(routes.Application.index)
+    }
+  }
+
+  def showCoupon(
+    couponDetail: CouponDetailWithMetadata
+  )(
+    implicit request: RequestHeader,
+    login: LoginSession
+  ): Result = {
+    couponDetail.siteItemNumericMetadata.get(SiteItemNumericMetadataType.COUPON_TEMPLATE) match {
+      case None => Ok(views.html.showCoupon(couponDetail))
+      case Some(metadata) =>
+        if (metadata.metadata == 0) Ok(views.html.showCoupon(couponDetail))
+        else Ok(views.html.showCouponTemplate(metadata.metadata, couponDetail))
+    }
   }
 }
