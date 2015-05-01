967102f5ef5b8b12eae2f991162d077ea1898d4c
Shisei Hanai <ruimo.uno@gmail.com>
2015-05-01 12:01:30 +0900

    Can refere employee in view.

--- app/helpers/ViewHelpers.scala
+++ app/helpers/ViewHelpers.scala
@@ -2,11 +2,18 @@ package helpers
 
 import java.util.Locale
 import play.api.i18n.Lang
+import play.api.db.DB
+import play.api.Play.current
+import models.{Employee, LoginSession}
 
 object ViewHelpers {
   def toAmount(amount: BigDecimal)(implicit lang: Lang): String = lang.toLocale match {
     case Locale.JAPANESE => String.format("%,.0f円", amount.bigDecimal)
     case Locale.JAPAN => String.format("%,.0f円", amount.bigDecimal)
     case _ => String.format("%.2f", amount.bigDecimal)
+  }
+
+  def employee(implicit optLogin: Option[LoginSession]): Option[Employee] = DB.withConnection { implicit conn =>
+    optLogin.flatMap { login => Employee.getBelonging(login.storeUser.id.get) }
   }
 }
