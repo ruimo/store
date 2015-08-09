51fc4d6e3b735c2f5006db2117f11de2c9842035
Shisei Hanai <ruimo.uno@gmail.com>
2015-08-10 07:37:12 +0900

    Fix ddl.

--- conf/evolutions/default/27.sql
+++ conf/evolutions/default/27.sql
@@ -5,7 +5,6 @@
 create table supplemental_category (
   item_id bigint not null references item on delete cascade,
   category_id bigint not null references category on delete cascade,
-  constraint pk_supplemental_category primary key (supplemental_category_id),
   unique (item_id, category_id)
 );
 
