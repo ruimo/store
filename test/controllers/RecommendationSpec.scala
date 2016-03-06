27891a43d2db6e7f547105f53586bdefa12c8a14
Shisei Hanai <ruimo.uno@gmail.com>
2016-03-06 19:52:53 +0900

    Fix test.

--- test/controllers/RecommendationSpec.scala
+++ test/controllers/RecommendationSpec.scala
@@ -18,34 +18,37 @@ import com.ruimo.recoeng.json.SalesItem
 class RecommendationSpec extends Specification {
   "Recommendation controller" should {
     "Can calculate recommendation by admin when item recommendation is empty." in {
-      val expectedDetails = Map(
-        (111L, 222L) -> mock(classOf[models.ItemDetail]),
-        (112L, 223L) -> mock(classOf[models.ItemDetail]),
-        (113L, 224L) -> mock(classOf[models.ItemDetail])
-      )
-      val details = Recommendation.calcByAdmin(
-        shoppingCartItems = List(),
-        maxCount = 5,
-        queryRecommendByAdmin = (recordCountToRetrieve: Int) => {
-          recordCountToRetrieve === 5
-          PagedRecords(
-            currentPage = 0,
-            pageSize = 10,
-            pageCount = 1,
-            orderBy = OrderBy("score", Desc),
-            records = Seq(
-              (RecommendByAdmin(Some(1L), 111L, 222L, 10, true), None, None),
-              (RecommendByAdmin(Some(2L), 112L, 223L, 20, true), None, None),
-              (RecommendByAdmin(Some(3L), 113L, 224L, 30, true), None, None)
+      val app = FakeApplication(additionalConfiguration = inMemoryDatabase())
+      running(TestServer(3333, app), Helpers.HTMLUNIT) { browser => DB.withConnection { implicit conn =>
+        val expectedDetails = Map(
+          (111L, 222L) -> mock(classOf[models.ItemDetail]),
+          (112L, 223L) -> mock(classOf[models.ItemDetail]),
+          (113L, 224L) -> mock(classOf[models.ItemDetail])
+        )
+        val details = Recommendation.calcByAdmin(
+          shoppingCartItems = List(),
+          maxCount = 5,
+          queryRecommendByAdmin = (recordCountToRetrieve: Int) => {
+            recordCountToRetrieve === 5
+            PagedRecords(
+              currentPage = 0,
+              pageSize = 10,
+              pageCount = 1,
+              orderBy = OrderBy("score", Desc),
+              records = Seq(
+                (RecommendByAdmin(Some(1L), 111L, 222L, 10, true), None, None),
+                (RecommendByAdmin(Some(2L), 112L, 223L, 20, true), None, None),
+                (RecommendByAdmin(Some(3L), 113L, 224L, 30, true), None, None)
+              )
             )
-          )
-        },
-        (siteId: Long, itemId: Long) => expectedDetails(siteId -> itemId)
-      )
-      details.size === 3
-      details(0) === expectedDetails(111L -> 222L)
-      details(1) === expectedDetails(112L -> 223L)
-      details(2) === expectedDetails(113L -> 224L)
+          },
+          (siteId: Long, itemId: Long) => expectedDetails(siteId -> itemId)
+        )
+        details.size === 3
+        details(0) === expectedDetails(111L -> 222L)
+        details(1) === expectedDetails(112L -> 223L)
+        details(2) === expectedDetails(113L -> 224L)
+      }}
     }
 
     "Can calculate recommendation by admin when there are some item recommendations." in {
