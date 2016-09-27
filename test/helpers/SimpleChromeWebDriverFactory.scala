a3118f244f1e15a0bfbee419c88fa59daaa7e09c
Shisei Hanai <ruimo.uno@gmail.com>
2016-09-27 17:12:54 +0900

    Fix test.

--- test/helpers/SimpleChromeWebDriverFactory.scala
+++ test/helpers/SimpleChromeWebDriverFactory.scala
@@ -1,11 +1,17 @@
 package helpers
 
+import org.openqa.selenium.remote.RemoteWebDriver
+import org.openqa.selenium.remote.LocalFileDetector
 import org.openqa.selenium.WebDriver
 
 class SimpleChromeWebDriverFactory extends SeleniumWebDriverFactory {
-  def create(): WebDriver = new org.openqa.selenium.remote.RemoteWebDriver(
-    new java.net.URL("http://127.0.0.1:4444/wd/hub"), SimpleChromeWebDriverFactory.caps
-  )
+  def create(): WebDriver = {
+    val wd = new org.openqa.selenium.remote.RemoteWebDriver(
+      new java.net.URL("http://127.0.0.1:4444/wd/hub"), SimpleChromeWebDriverFactory.caps
+    )
+    wd.setFileDetector(new LocalFileDetector)
+    wd
+  }
 }
 
 object SimpleChromeWebDriverFactory {
